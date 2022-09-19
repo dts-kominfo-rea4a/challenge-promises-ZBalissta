03058a24bb8c0381bbc78968970419e5ebd51bfb
ZBalissta <salimystogan69@gmail.com>
2022-09-19 16:27:26 +0700

    result emosi

--- promise.js
+++ promise.js
@@ -1,8 +1,26 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+
+  const allTheater = theaterIXX.concat(theaterVGC)
+
+  let emosiViewer = 0;
+
+  allTheater.forEach((film) => {
+    if (film.hasil === emosi) {
+      emosiViewer++;
+    }
+  })
+  return emosiViewer;
+};
+
+console.log(promiseOutput('marah'))
+
 
 module.exports = {
   promiseOutput,
 };
+
